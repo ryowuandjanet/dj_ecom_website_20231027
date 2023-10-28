aeb64ea3cd3921e454ccdfe1ab8a5f6bf19d8111
ryowuandjanet <ryowu0329@gmail.com>
2023-10-29 01:41:08 +0800

    Part 5-Home View

--- product/urls.py
+++ product/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from .views import (
   Home,
-  ProductDetail
+  ProductDetails
 )
 
 urlpatterns = [
   path('',Home.as_view(),name='home'),
-  path('product-details/',ProductDetail.as_view(),name='product-details')
+  path('product-details/',ProductDetails.as_view(),name='product-details')
 ]
--- product/views.py
+++ product/views.py
@@ -1,7 +1,25 @@
+from typing import Any, Dict
 from django.views import generic
+
+from .models import (
+  Category,
+  Product,
+  Slider
+)
 
 class Home(generic.TemplateView):
   template_name = 'home.html'
 
-class ProductDetail(generic.TemplateView):
+  def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+    context =  super().get_context_data(**kwargs)
+    context.update(
+      {
+        'featured_categories': Category.objects.filter(featured = True),
+        'featured_products': Product.objects.filter(featured = True),
+        'sliders': Slider.objects.filter(show=True)
+      }
+    )
+    return context
+
+class ProductDetails(generic.TemplateView):
   template_name = 'product/product-details.html'
