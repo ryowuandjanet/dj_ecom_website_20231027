c6cc5df7518d646b60bff913459831f160fb0a1a
ryowuandjanet <ryowu0329@gmail.com>
2023-10-29 13:50:03 +0800

    Part 11-Update Cart

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,5 @@
 from django.views import generic
+from django.contrib import messages
 from django.shortcuts import get_object_or_404,redirect
 from .carts import Cart
 from product.models import Product
@@ -12,3 +13,28 @@ class AddToCart(generic.View):
 
 class CartItems(generic.TemplateView):
     template_name = 'cart/cart.html'
+
+    def get(self, request, *args, **kwargs):
+        product_id = request.GET.get('product_id', None)
+        quantity = request.GET.get('quantity',None)
+        clear = request.GET.get('clear',False)
+        cart = Cart(request)
+
+        if product_id and quantity:
+            product = get_object_or_404(Product,id=product_id)
+            if int(quantity) > 0:
+                if product.in_stock:
+                    cart.update(int(product_id),int(quantity))
+                    return redirect('cart')
+                else:
+                    messages.warning(request, "Your account expires in three days.")
+                    return redirect('cart')
+            else:
+                cart.update(int(product_id),int(quantity))
+                return redirect('cart')
+        
+        if clear:
+            cart.clear()
+            return redirect('cart')
+        
+        return super().get(request, *args, **kwargs)
